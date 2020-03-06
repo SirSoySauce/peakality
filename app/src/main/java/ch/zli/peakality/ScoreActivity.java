4dbd7801b3db015ea45482ef9d26039a5e84ab16
Chris Wolf <39116095+GRcwolf@users.noreply.github.com>
2020-03-06 14:06:26 +0100

    Updated layout of values

--- app/src/main/java/ch/zli/peakality/ScoreActivity.java
+++ app/src/main/java/ch/zli/peakality/ScoreActivity.java
@@ -3,8 +3,8 @@ package ch.zli.peakality;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.View;
+import android.widget.TextView;
 
 import ch.zli.peakality.database.entity.Score;
 
@@ -17,6 +17,18 @@ public class ScoreActivity extends Activity {
     public static final String SCORE_EXTRA_NAME = "score_extra";
 
     FloatingActionButton shareScoreButton;
+
+    TextView positionView;
+
+    TextView temperatureView;
+
+    TextView pressureView;
+
+    TextView altitudeView;
+
+    TextView dateView;
+
+    TextView weatherView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +44,13 @@ public class ScoreActivity extends Activity {
                 Intent sharingIntent = new Intent(Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
                 // @TODO: Set actual score.
-                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "My score is: PLACE_HOLDER_SCORE");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "My score is: " + score.getAirPressure());
                 startActivity(Intent.createChooser(sharingIntent, "Share using"));
             }
         });
+    }
+
+    private void updateMeasuredValues(Score score) {
+
     }
 }
