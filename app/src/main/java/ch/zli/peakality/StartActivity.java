a2dc7bb611eb4a204a86a6cc618b3801aeab39cf
Chris Wolf <39116095+GRcwolf@users.noreply.github.com>
2020-03-06 12:48:29 +0100

    Added navigation to score activity

--- app/src/main/java/ch/zli/peakality/StartActivity.java
+++ app/src/main/java/ch/zli/peakality/StartActivity.java
@@ -1,17 +1,28 @@
 package ch.zli.peakality;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends Activity {
+
+    Button generateScoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        generateScoreButton = findViewById(R.id.bGenerateScore);
+
+        generateScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, ScoreActivity.class));
+            }
+        });
     }
 }
