a74ea148b65f6526012b59f8fbcda35919a4659c
Chris Wolf <39116095+GRcwolf@users.noreply.github.com>
2020-03-06 13:46:13 +0100

    Implemented score sharing

--- app/src/main/java/ch/zli/peakality/ScoreActivity.java
+++ app/src/main/java/ch/zli/peakality/ScoreActivity.java
@@ -1,13 +1,32 @@
 package ch.zli.peakality;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ScoreActivity extends Activity {
+
+    FloatingActionButton shareScoreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        shareScoreButton = findViewById(R.id.fabShareScore);
+
+        shareScoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                // @TODO: Set actual score.
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, "My score is: PLACE_HOLDER_SCORE");
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+            }
+        });
     }
 }
