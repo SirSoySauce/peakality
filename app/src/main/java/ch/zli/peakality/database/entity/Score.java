2ad4f4d338bc68866514cd4f55fb4e077daf6571
Ivo Cavelti <ivo.cavelti@bluewin.ch>
2020-03-06 11:09:24 +0100

    Added DAO and Entry content

--- app/src/main/java/ch/zli/peakality/database/entity/Score.java
+++ app/src/main/java/ch/zli/peakality/database/entity/Score.java
@@ -1,14 +1,30 @@
 package ch.zli.peakality.database.entity;
 
-import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Score {
 
-    @PrimaryKey
-    @ColumnInfo(name = "score_id")
-    public int scoreId;
+    @PrimaryKey(autoGenerate = true)
+    private int scoreId;
+
+    public String longitude;
+
+    public String latitude;
+
+    public String altitude;
+
+    public double temperature;
+
+    public String weather;
+
+    public double windSpeed;
+
+    public double airPressure;
+
+    public Score(int scoreId) {
+        this.scoreId = scoreId;
+    }
 
 }
